1ae1c8b8f14440f04b680b731645368055a8e9e0
Sheetal <sheetal1060@gmail.com>
2021-11-09 20:40:33 +0530

    compete

--- courses.php
+++ courses.php
@@ -2,112 +2,33 @@
 <html lang="en">
 
 <head>
-  <title>registration</title>
+  <title>courses</title>
   <?php include "css/index.php" ?>
   <?php include "links/links.php" ?>
-
-
+<style>
+.video{
+    margin-top: 10%;
+}
+</style>
 </head>
-</body>
-<?php include "header.php" ?>
-  <main id="main" data-aos="fade-in">
-
-    
-    <div class="breadcrumbs">
-      <div class="container">
-        <h2>Courses</h2>
-        <p>Start learning with us </div>
-    </div>
-
-    <section id="courses" class="courses">
-      <div class="container" data-aos="fade-up">
-
-        <div class="row" data-aos="zoom-in" data-aos-delay="100">
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-            <div class="course-item">
-              <img src="assets/img/course-1.jpg" class="img-fluid" alt="...">
-              <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                  <h4>Web Development</h4>
-                  
-                </div>
-
-                <h3><a href="course-details.html">Website Design</a></h3>
-                
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="trainer-1.jpg" class="img-fluid" alt="">
-                    <span>Antonio</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> 
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-            <div class="course-item">
-              <img src="assets/img/course-2.jpg" class="img-fluid" alt="...">
-              <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                  <h4>Marketing</h4>
-                 
-                </div>
-
-                <h3><a href="course-details.html"></a></h3>
-                <p></p>
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="assets/img/trainers/trainer-2.jpg" class="img-fluid" alt="">
-                    <span>Lana</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> 
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-            <div class="course-item">
-              <img src="assets/img/course-3.jpg" class="img-fluid" alt="...">
-              <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                  <h4>Content</h4>
-                  <p class="price">$180</p>
-                </div>
-
-                <h3><a href="course-details.html">Copywriting</a></h3>
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="assets/img/trainers/trainer-3.jpg" class="img-fluid" alt="">
-                    <span>Brandon</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
+<body>
+<?php include "header.php" ?><hr>
+<br><br><br>
+<div class="container mt-3 mb-3 video ">
+    <div class="row">
+        <?php include 'dbcon.php';
+        $q='select *from video';
+        $query=mysqli_query($con,$q);
+        while($row=mysqli_fetch_array($query)){
+        ?>
+        <div class="col-md-4">
+            <video id="myVideo" width='100%' controls>
+                <source src="<?php echo 'upload/'.$row['name'];?>">
+            </video>
         </div>
-
-      </div>
-    </section>
-  </main>
-
-  <?php include "footer.php"; ?>
+        <?php }
+        ?>
+    </div>
+</div><?php include "footer.php"; ?>
 </body>
 </html>
